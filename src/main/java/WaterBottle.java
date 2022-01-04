d413b420a1683e3638109876f6a91c4369cf4926
Anita Safar <ancsa0416@gmail.com>
2022-01-04 18:04:05 +0000

    fill function and notempty added

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -5,11 +5,19 @@ public class WaterBottle {
         this.volume = volume;
     }
 
+   public int notEmpty(){
+        return this.volume;
+   }
+
    public int drink(){
         return this.volume - 10;
    }
 
    public int empty(){
         return this.volume = 0;
+   }
+
+   public int fill(){
+        return this.volume = 100;
    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -13,6 +13,11 @@ public class WaterBottleTest {
     }
 
     @Test
+    public void bottleNotEmpty(){
+        assertEquals(100, water.notEmpty());
+    }
+
+    @Test
     public void canDrink(){
         assertEquals(90, water.drink());
     }
@@ -20,5 +25,10 @@ public class WaterBottleTest {
     @Test
     public void bottleIsEmpty(){
         assertEquals(0, water.empty());
+    }
+
+    @Test
+    public void bottleIsFilled(){
+        assertEquals(100, water.fill());
     }
 }
