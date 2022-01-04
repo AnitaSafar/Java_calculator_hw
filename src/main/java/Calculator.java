28afa17f6b204500909ba488e74ad22b7287678b
Anita Safar <ancsa0416@gmail.com>
2022-01-04 16:55:12 +0000

    secondnumber test added

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,11 +1,17 @@
 public class Calculator {
     private int firstNumber;
+    private int secondNumber;
 
-    public Calculator(int firstNumber){
+    public Calculator(int firstNumber, int secondNumber){
         this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
     }
 
     public int getFirstNumber(){
         return this.firstNumber;
+    }
+
+    public int getSecondNumber(){
+        return this.secondNumber;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,11 +8,17 @@ public class CalculatorTest {
     Calculator myCalculator;
     @Before
     public void before(){
-        myCalculator = new Calculator(10);
+        myCalculator = new Calculator(10, 5);
     }
 
     @Test
     public void hasFirstNumber(){
         assertEquals(10, myCalculator.getFirstNumber());
     }
+
+    @Test
+    public void hasSecondNumber(){
+        assertEquals(5, myCalculator.getSecondNumber());
+    }
+    
 }
