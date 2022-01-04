b1078286f5b19510a78c954512cbea875683a168
Anita Safar <ancsa0416@gmail.com>
2022-01-04 18:20:17 +0000

    printer and printertest added, haspaper test done

---
+++ src/main/java/Printer.java
@@ -0,0 +1,11 @@
+public class Printer {
+    private int sheets;
+
+    public Printer(int sheets){
+        this.sheets = sheets;
+    }
+
+    public int getPaper(){
+        return this.sheets;
+    }
+}
---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrinterTest {
+
+    Printer myPrinter;
+
+    @Before
+    public void before(){
+        myPrinter = new Printer(50);
+    }
+
+    @Test
+    public void hasPaper(){
+        assertEquals(50, myPrinter.getPaper());
+    }
+
+    @Test
+    public void
+}
